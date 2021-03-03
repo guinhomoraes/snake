4cfcffc328e7ad5fd7011e52510b6989732d8bf0
guinhomoraes <guilhermeguilherme98@gmail.com>
2021-03-02 21:10:49 -0300

    Movimentação da cobrinha

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,13 @@ snake[0] =
     x: 8 * box,
     y: 8 * box
 }
+
 let direction = "right"; 
+let food = 
+{
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG()
 {
@@ -24,10 +30,36 @@ function criarCobrinha()
     }
 }
 
+function drawFood()
+{
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown',update);
+
+function update(event)
+{
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+
+}
+
 function iniciarJogo()
 {
+    console.log("entrou");
+
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -48,5 +80,9 @@ function iniciarJogo()
     snake.unshift(newHead);
 }
 
- let jogo = setInterval(iniciarJogo(),100);
+setInterval(function()
+{
+    iniciarJogo(); 
+}, 100);
+
 
